b91acc10d7294ffe51686588ef0f4b6fd32148ab
Alba2907 <kolevlo@yandex.ru>
2023-01-14 13:45:36 +0300

    header

---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="header">
+    <div class="row">
+      <div class="title col-1"><h1>Alba</h1></div>
+      <div class="search col-10 ">
+        <input class="search-input" placeholder="Search">
+      </div>
+      <div class="filter col-1">
+        <div class="row">
+          <div class="filter-icon col"><img src="../assets/filter.svg"></div>
+          <div class="filter-name col">Filter</div>
+        </div>
+
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <TheHeader />
+
+</template>
+
+<script>
+// @ is an alias to /src
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TheHeader
+  }
+}
+</script>
